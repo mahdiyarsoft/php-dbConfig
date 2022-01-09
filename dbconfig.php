a067b8e6f520c2cd780384dec6e8395b8b503d7d
Mahdiyar Sahbaee Ahmadi <78661695+mahdiyarsoft@users.noreply.github.com>
2022-01-09 14:26:06 +0330

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,9 +1,9 @@
 <?php
-// MAHDIYAR SAHBAEE AHMADI
-$DNS = "mysql";         //Database type.
-$HOST = "localhost";    //The host where the database is located #default: "localhost".
-$DBNAME="###";     //The database you want to connect to #databaseName.
-$dbUSERNAME="root";     // database userName #default: "root".
-$dbPASSWORD="";         // database userName #default: "null".
-$con = new  PDO("$DNS:host=$HOST;dbname=$DBNAME;charset=utf8", "$dbUSERNAME", "$dbPASSWORD");
+  // MAHDIYAR SAHBAEE AHMADI
+  $DNS = "mysql";          //Database type.
+  $HOST = "localhost";     //The host where the database is located #default: "localhost".
+  $DBNAME= "###";          //The database you want to connect to #databaseName.
+  $dbUSERNAME= "root";     // database userName #default: "root".
+  $dbPASSWORD= "";         // database userName #default: "null".
+  $con = new  PDO("$DNS:host=$HOST;dbname=$DBNAME;charset=utf8", "$dbUSERNAME", "$dbPASSWORD");
 ?>
